e150c14c00b40a535937157ffba7b2fc552ea252
SD <m.simardavid@gmail.com>
2023-10-18 10:26:32 +0900

    imges

---
+++ assets/text/gachiFrench.js
@@ -0,0 +1,62 @@
+export const gachiScreenShortDescriptionFrench = [
+  {
+    title: "PAGE PRINCIPALE",
+    subTitle: "Une page qui fournit des informations personnalisées après ma connexion.",
+    imgSrc: "https://simdav888.github.io/assets/commandBoard.76436257.png",
+    footer:
+      "Sur la page principale, il me montre clairement ce que je dois faire aujourd'hui en rouge. Les autres membres du groupe auquel j'appartiens peuvent voir les objectifs et les tâches qu'ils ont accomplis, ce qui est motivant.",
+    ActionAreaTitle: "Page principale",
+    description: `Résumé de l'activité du réseau`,
+    imageUrl: "https://simdav888.github.io/assets/commandBoard.76436257.png",
+  },
+  {
+    title: "MON PROFIL",
+    subTitle: "Configurer et modifier mes informations personnelles.",
+    imgSrc: "https://simdav888.github.io/assets/profile.3890dac5.png",
+    footer:
+      "Dans mon profil, je peux télécharger des photos et modifier des informations. Si je ne veux pas que mes informations soient visibles par les autres membres, je peux les définir comme privées. Je peux également ajuster la visibilité de mes questions, réponses et objectifs au sein des groupes auxquels j'appartiens.",
+    ActionAreaTitle: "Mon profil",
+    description: `Paramètres personnels`,
+    imageUrl: "https://simdav888.github.io/assets/profile.3890dac5.png",
+  },
+  {
+    title: "CRÉER MON OBJECTIF",
+    subTitle: "Créer mes propres objectifs.",
+    imgSrc: "https://simdav888.github.io/assets/goalMaker.3597cf5a.png",
+    footer:
+      "Je peux définir une date limite pour mes objectifs personnels, ce qui crée des tâches quotidiennes dans mon agenda. Lorsque je termine une tâche, elle apparaît sur le fil d'actualité du groupe que j'ai choisi, et mon taux de réalisation des objectifs augmente.",
+    ActionAreaTitle: "Créer mon objectif",
+    description: `Formulaire de saisie`,
+    imageUrl: "https://simdav888.github.io/assets/goalMaker.3597cf5a.png",
+  },
+  {
+    title: "MES PLANS & AGENDAS",
+    subTitle: "Liste de mes objectifs et de mon emploi du temps.",
+    imgSrc: "https://simdav888.github.io/assets/Plans.ded3fd9c.png",
+    footer:
+      "C'est un espace où je peux voir les objectifs que j'ai créés. Je peux créer plusieurs objectifs et les afficher sous forme de liste. Selon la manière dont je les crée, cela peut générer plusieurs tâches quotidiennes dans mon agenda.",
+    ActionAreaTitle: "Mes plans & agendas",
+    description: `Affichage de liste de données`,
+    imageUrl: "https://simdav888.github.io/assets/Plans.ded3fd9c.png",
+  },
+  {
+    title: "RÉSEAU",
+    subTitle: "Page du réseau",
+    imgSrc: "https://simdav888.github.io/assets/network.7b10601d.png",
+    footer:
+      "Je peux voir une liste de groupes auxquels je ne participe pas. Je peux entrer dans un groupe pour voir son contenu et le rejoindre immédiatement. Dans les groupes auxquels je me suis déjà joint, je peux voir une liste des questions que j'ai posées.",
+    ActionAreaTitle: "Réseau",
+    description: `Système de notification`,
+    imageUrl: "https://simdav888.github.io/assets/network.7b10601d.png",
+  },
+  {
+    title: "LECTURE DU GROUPE",
+    subTitle: "Afficher les détails du groupe.",
+    imgSrc: "https://simdav888.github.io/assets/circleRead.c257fcae.png",
+    footer:
+      "Je peux voir les membres du groupe. Sur le fil d'actualité, je peux voir diverses mises à jour liées aux objectifs des membres. Je peux poser librement des questions liées au sujet du groupe, fournir des réponses et aimer les commentaires. Si je suis le leader du groupe, je peux supprimer les questions inappropriées et exclure des membres si nécessaire. Je peux également modifier les informations du groupe.",
+    ActionAreaTitle: "Groupe",
+    description: `Système de fil d'actualité`,
+    imageUrl: "https://simdav888.github.io/assets/circleRead.c257fcae.png",
+  },
+];
